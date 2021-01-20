14a41d888361972bfe3c857f1a59b88d5e543ef8
Haedong-Jeon <haedongjeon@icloud.com>
2021-01-20 12:17:34 +0900

    백엔드에 주문 저장

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'package:ShopApp/models/http_exceptions.dart';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +19,31 @@ class Product with ChangeNotifier {
     @required this.imgURL,
     this.isFavorite = false,
   });
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newStatus) {
+    isFavorite = newStatus;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final bool oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final String url =
+        'https://flutter-shopapp-9b7b2-default-rtdb.firebaseio.com/products/${id}.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode(
+          {
+            'isFavorite': isFavorite,
+          },
+        ),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(isFavorite);
+    }
   }
 }
